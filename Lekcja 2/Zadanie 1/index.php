232341b2697a13dfb3c6e2a1027da86dcdc6e90f
KaroL2705 <swierczkarol27@gmail.com>
2021-12-09 09:42:15 +0100

    ver 0.2

---
+++ Lekcja 2/Zadanie 1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 1</title>
+</head>
+<body>
+    <table> 
+        <?php
+            for($h=1; $h<=100; $h++){
+                echo "<tr>\n";
+
+                for ($w=1; $w<=100; $w++){
+                    echo "<td>";
+                    echo $h*$w;
+                    echo "</td>\n";
+                }
+                echo "</tr>\n";
+            }
+        ?>
+    </table>
+</body>
+</html>
