6c02d2b1cfd723d053c54878e2509436a13febe0
KaroL2705 <swierczkarol27@gmail.com>
2021-12-16 09:53:17 +0100

    Lekcja 3.0.4

--- Lekcja 3/index.php
+++ Lekcja 3/index.php
@@ -9,9 +9,20 @@
 </head>
 <body>
     <?php
-        $connect= mysqli_connect([localhost[,root[,[Swiercz]]]]);
-        or die('Nie mogę połączyć się z bazą danych<br>'.mysqli_error());
-        echo "Udało się połączyć z bazą dancyh";
+        $connect=mysqli_connect('localhost','root','','Swiercz') or die('Nie mogę połączyć się z bazą danych<br>'.mysqli_error());
+        echo "Udało się połączyć z bazą dancyh.<br>";
+        //$DB= 'CREATE TABLE TEST(id int not null, nazwisko char(30), lata char(3))';
+        //$zapytanie =mysqli_query($connect,$DB) or die ('nie udało się utworzyć tabeli');
+        //echo "Udało się utowrzyć tabelę<br> $zapytanie";
+        $wybór= 'SELECT nazwiska,lata from TEST';
+        $idzapytania= mysqli_query($connect,$wybór);
+        echo '<table>'; 
+        while ($wiersz = mysqli_fetch_row($idzapytania)){ 
+            echo '<tr><td>Nazwisko: '. $wiersz[0] .'</td><td>Lata: '. $wiersz[1] .'</td></tr>'; 
+        }
+    echo '</table>';
+
+
     ?>   
 </body>
 </html>
