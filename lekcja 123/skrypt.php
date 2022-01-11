7acb5588b955306746a6b8f0cf2fccc0e37382de
KaroL2705 <swierczkarol27@gmail.com>
2022-01-11 13:04:31 +0100

    lekcja 4 v 0.1

---
+++ lekcja 123/skrypt.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+    Imię: <?php echo $_GET['imie']?><br/> 
+    Nazwisko: <?php echo $_GET['nazwisko']?><br/> 
+    Data urodzenia <?php echo $_GET['data']?><br>
+    Miasto <?php echo $_GET['miasto']?><br>
+    Wiek <?php echo $_GET['wiek']?><br>
+</body>
+</html>
